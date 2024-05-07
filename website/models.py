a74c415a517a985b5f2f12f744652ec2c1cbff79
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-07 21:33:53 +0300

    database update

--- website/models.py
+++ website/models.py
@@ -6,9 +6,12 @@ from django.db import models
 class Task(models.Model):
     content = models.TextField(max_length=500, blank=False)
     start_date = models.DateTimeField(default=datetime.now)
-    deadline = models.DateTimeField()
+    deadline = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tags", blank=True, related_name="tasks")
+
+    class Meta:
+        ordering = ["status"]
 
 
 class Tags(models.Model):
