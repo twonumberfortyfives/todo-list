0fe8a222028bec2794221c829dc1e81a72c40330
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-07 10:33:28 +0300

    sidebar and base html have been implemented

--- website/views.py
+++ website/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, template_name="base.html")
