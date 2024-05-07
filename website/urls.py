1518960922d2103d817450874dbe3a6108c8c894
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-07 12:35:29 +0300

    create, list view have been implemented

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from website import views
+from website.views import (
+    index,
+    TaskCreateView,
+    TaskListView,
+)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', index, name='index'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('create-task/', TaskCreateView.as_view(), name='task-create'),
+
 ]
 
 app_name = 'website'
--- website/views.py
+++ website/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views import generic
+
+from website.models import Task
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, template_name="base.html")
+    return render(request, template_name="home.html")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    template_name = "task_form.html"
+    fields = "__all__"
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "home.html"
